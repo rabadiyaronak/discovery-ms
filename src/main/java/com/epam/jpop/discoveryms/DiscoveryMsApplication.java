d1ad26cb3fcbcc1ae837d907a4da886c02e467b8
Ronak_Rabadiya <rabadiyaronak@gmail.com>
2021-06-21 14:03:04 +0530

    discovery-service | add configuration for discovery service

--- src/main/java/com/epam/jpop/discoveryms/DiscoveryMsApplication.java
+++ src/main/java/com/epam/jpop/discoveryms/DiscoveryMsApplication.java
@@ -2,7 +2,9 @@ package com.epam.jpop.discoveryms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryMsApplication {
 
