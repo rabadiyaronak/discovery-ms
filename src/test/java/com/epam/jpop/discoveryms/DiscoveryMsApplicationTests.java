893f2b813a7720d0518475a93b265ec08945c9ae
Ronak_Rabadiya <rabadiyaronak@gmail.com>
2021-06-21 13:44:48 +0530

    initial commit

---
+++ src/test/java/com/epam/jpop/discoveryms/DiscoveryMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.jpop.discoveryms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
